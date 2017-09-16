615e5907540877bbbc1a201e908c51d06c6ac4b6
caleb logan <clogan202@gmail.com>
2017-09-16 11:38:30 -0700

    add prod db settings

--- element_watch/config/settings/prod.py
+++ element_watch/config/settings/prod.py
@@ -3,5 +3,17 @@ from .base import *
 
 DEBUG = False
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['POSTGRES_NAME'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': os.environ['POSTGRES_PORT']
+    }
+}
+
+
 CELERY_BROKER_READ_URL = os.environ['RABBITMQ_BIGWIG_RX_URL']
 CELERY_BROKER_WRITE_URL = os.environ['RABBITMQ_BIGWIG_TX_URL']
